4f150b3cd5d254e78350c02a108fad25f3e167f9
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 22:26:31 +0300

    Add project files.

---
+++ ReadFolder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n  Path : ");
+            string path = Console.ReadLine();
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            Console.WriteLine("\n\n  Directories : ");
+            DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+            for (int i = 0; i < directoryInfos.Length; i++)
+                Console.WriteLine(directoryInfos[i].Name);
+
+            Console.WriteLine("\n\n  Files : ");
+            FileInfo[] fileInfos = directoryInfo.GetFiles();
+            for (int i = 0; i < fileInfos.Length; i++)
+                Console.WriteLine(fileInfos[i].Name);
+
+            Console.Write("\n\n  File to start : ");
+            string file = Console.ReadLine();
+            Process.Start(path + @"\" + file);
+        }
+    }
+}
